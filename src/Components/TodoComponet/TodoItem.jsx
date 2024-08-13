8286087a0e62788a87995845f115c4f526e9c650
Amit Pal <Amit.Pal@teknopoint.local>
2024-08-13 18:26:22 +0530

    changes

--- src/Components/TodoComponet/TodoItem.jsx
+++ src/Components/TodoComponet/TodoItem.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { useTodo } from '../../Context/TodoContextFol/TodoContext';
 
 function TodoItem({ todo }) {
+    console.log("test",todo);
+    
     const [isTodoEditable, setIsTodoEditable] = useState(false)
     const [todoMsg, setTodoMsg] = useState(todo?.todo)
     const { updateTodo, deleteTodo, toggleComplete } = useTodo()
